753cf5d7809305ab4e55a9b432fc9ba77dc3be20
Avinash <ak21367185@gmail.com>
2025-04-18 21:21:07 +0530

    added register page

--- VendorPro/app/_layout.tsx
+++ VendorPro/app/_layout.tsx
@@ -1,7 +1,42 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { Slot, useRouter, useSegments } from 'expo-router';
+import { useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+// Define session context type
+type User = {
+  name: string;
+  mobile: string;
+};
+
+export const unstable_settings = {
+  initialRouteName: '(auth)/register',
+};
+
+function useProtectedRoute() {
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const userSession = await AsyncStorage.getItem('user');
+      const isAuth = segments[0];
+      
+      if (!userSession && isAuth !== '(auth)') {
+        // Redirect to the sign-in page if not authenticated
+        router.replace('/register');
+      } else if (userSession && isAuth === '(auth)') {
+        // Redirect to the dashboard if authenticated
+        router.replace('/dashboard');
+      }
+    };
+
+    checkAuth();
+  }, [segments]);
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  useProtectedRoute();
+  return <Slot />;
 }
